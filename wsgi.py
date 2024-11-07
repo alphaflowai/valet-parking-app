f72401e477e09ebc6a7001a9945017f1558c767f
sendtocomplete <150406323+sendtocomplete@users.noreply.github.com>
2024-11-07 12:48:38 -0500

    Add Amplify configuration files

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
-from app import create_app, socketio
+from app import create_app
+from app import socketio
 
 app = create_app()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     socketio.run(app) 
