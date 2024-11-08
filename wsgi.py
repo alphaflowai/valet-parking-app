a25dec6ae03a8d0722e7e05e7acf6ad14485ad06
sendtocomplete <150406323+sendtocomplete@users.noreply.github.com>
2024-11-08 16:20:38 -0500

    add socket update

--- wsgi.py
+++ wsgi.py
@@ -4,8 +4,9 @@ eventlet.monkey_patch()
 from app import create_app, socketio
 
 app = create_app()
-socketio.init_app(app)
-application = app.wsgi_app
+socketio.init_app(app, async_mode='eventlet')
+# Create WSGI middleware
+application = socketio.get_wsgi_app(app)
 
 if __name__ == '__main__':
     socketio.run(app) 
