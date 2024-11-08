3c414af925fecd54f52290bddc363344bf2cf394
sendtocomplete <150406323+sendtocomplete@users.noreply.github.com>
2024-11-08 12:03:17 -0500

    This is the correct way to wrap the Flask application with SocketIO middleware for use with Gunicorn

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ eventlet.monkey_patch()
 from app import create_app, socketio
 
 app = create_app()
-application = socketio.wsgi_app(app)
+application = socketio.middleware(app)
 
 if __name__ == '__main__':
     socketio.run(app) 
